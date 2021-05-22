4ff3b4291e75ce19e45751d8f0d5507323a71f58
Bryan Monroy <a01026848@itesm.mx>
2021-05-21 21:16:32 -0500

    Connection back with front

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,42 +1,68 @@
 import Logo from "../logo/logopng.png";
+import { useState, useEffect } from "react";
 import Search from "../logo/lupa.png";
 import React from "react";
 import styles from "./HeaderStyles.module.scss";
-import { BiSearchAlt } from "react-icons/bi"
-const Header = () => {
+import { BiSearchAlt } from "react-icons/bi";
+
+const Header = (props) => {
+  const [selection, setSelection] = useState("Movie name");
+
+  const [searchBar, setSearchBar] = useState("");
+
+  const handleOnChange = (e) => {
+    console.log("search bar changed");
+    console.log(e.target.value);
+    setSearchBar(e.target.value);
+  };
+
+  const handleSelection = (e) => {
+    console.log("Selection changed");
+    console.log(e.target.value);
+    setSelection(e.target.value);
+  };
+
+  const handleOnSubmit = () => {
+    props.sendQuery(selection, searchBar);
+  };
+
   return (
     <div className={styles.Wrapper}>
       <div className={styles.LogoWrapper}>
-
         <img src={Logo} className={styles.Logo}></img>
         {/* <SiNetflix className={styles.Icon} /> */}
       </div>
 
       <div className={styles.InputContainer}>
-
         <input
           placeholder="Search"
           className={styles.InputBar}
+          value={searchBar}
+          onChange={handleOnChange}
         />
-        <select className={styles.Drop}>
+        <select
+          className={styles.Drop}
+          value={selection}
+          onChange={handleSelection}
+        >
           <optgroup label="Movies">
-            <option value="By movie">By movie</option>
+            <option value="Movie">By movie</option>
           </optgroup>
           <optgroup label="Actors">
-            <option value="By Actor">By actor</option>
+            <option value="cast">By actor</option>
           </optgroup>
           <optgroup label="TV show">
-            <option value="By show name">By show name</option>
+            <option value="TV Show">By show name</option>
           </optgroup>
-          <optgroup label="Totals">
-            <option value="Movies by country">Total movies by country</option>
-            <option value="Movies and TV show">Movies and TV show</option>
-            <option value="TV show by release year">
-              TV show by release year
-            </option>
+          <optgroup label="Stats">
+            <option value="Stats,Country">Total movies by country</option>
+            <option value="Stats,Titles">Movies and TV shows</option>
+            <option value="Stats,Year">TV shows by release year</option>
           </optgroup>
         </select>
-        <BiSearchAlt src={Search} className={styles.Icon} />
+        <button className={styles.Button} onClick={handleOnSubmit}>
+          <BiSearchAlt src={Search} className={styles.Icon} />
+        </button>
       </div>
     </div>
   );
